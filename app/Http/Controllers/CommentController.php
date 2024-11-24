2df1a1466282a4a6e92bcd01032d7f546a56d99e
CZsyn <chakibzeraia@gmail.com>
2024-11-24 10:52:36 +0100

    fin des notifications

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -29,31 +29,30 @@ class CommentController extends Controller
     public function store(Request $request, $item_id)
     {
         if (!auth()->user()->hasVerifiedEmail()) {
-
             $errorAuthMessage = DB::table('success_messages')
                 ->where('key', 'user_unverify_comment')
                 ->value('message');
-
+    
             $this->flasher->addInfo($errorAuthMessage, 'Verify Now!', [
                 'position' => 'top-center',
                 'timer' => 5000,
                 'icon' => 'check-circle'
             ]);
-
+    
             return redirect()->route('account');
         }
-
+    
         $commentDelay = 60;
-
+    
         $errorDelayMessage = DB::table('success_messages')
             ->where('key', 'error_delay_comment')
             ->value('message');
-
+    
         $lastComment = Comment::where('user_id', auth()->id())
             ->where('item_id', $item_id)
             ->latest()
             ->first();
-
+    
         if ($lastComment && $lastComment->created_at->diffInSeconds(now()) < $commentDelay) {
             $this->flasher->addError($errorDelayMessage, 'Error!', [
                 'position' => 'top-center',
@@ -61,68 +60,74 @@ class CommentController extends Controller
                 'icon' => 'check-circle',
                 'theme' => 'light'
             ]);
-
+    
             return back();
         }
-
+    
         if (strlen($request->content) < 5) {
             $errorMinMessage = DB::table('success_messages')
                 ->where('key', 'error_min_comment')
                 ->value('message');
-
-
+    
             $this->flasher->addError($errorMinMessage, 'Error!', [
                 'position' => 'top-center',
                 'timer' => 5000,
                 'icon' => 'check-circle',
                 'theme' => 'light'
             ]);
-
+    
             return back();
         }
-
-
+    
         $validation = $request->validate([
             'content' => 'required|string|max:300'
         ]);
-
+    
         $successMessage = DB::table('success_messages')
             ->where('key', 'comment_post_success')
             ->value('message');
-
-        $create = Comment::create([
+    
+        $comment = Comment::create([
             'content' => $request->content,
             'user_id' => Auth::id(),
             'item_id' => $item_id,
             'status' => $this->new_comments
         ]);
-
-        if ($create) {
-
+    
+        if ($comment) {
+            $item = Item::find($item_id);
+    
+            // Vérifier si le propriétaire de l'item a activé les notifications
+            $itemOwner = $item->user; // Assurez-vous que l'item a une relation avec l'utilisateur
+    
+            if ($itemOwner && $itemOwner->notification_comment) {
+                // Envoyer une notification ou un email
+                $itemOwner->notify(new \App\Notifications\CommentNotification($comment));
+            }
+    
             if ($this->new_comments == 1) {
                 return back()->with('success', 'Your comment is posted.');
             } else {
-
                 $this->flasher->addInfo($successMessage, 'Success!', [
                     'position' => 'top-center',
                     'timer' => 5000,
                     'icon' => 'check-circle',
                     'theme' => 'light'
                 ]);
-
+    
                 return back();
             }
         } else {
-
             $this->flasher->addError(__('app.error_message'), 'Success!', [
                 'position' => 'top-center',
                 'timer' => 5000,
                 'icon' => 'check-circle'
             ]);
-
+    
             return back();
         }
     }
+    
 
     public function loadMore(Request $request, Item $item)
     {
